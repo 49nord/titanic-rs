49b2f7dc7b01f5eb7422cfbeee9a0aea8c327ebd
Carl Scherer <Lireer@users.noreply.github.com>
2018-05-28 23:19:43 +0200

    Document try macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,10 @@
+/// Like the try! macro, but only works on Result<Option<_>, _> and returns
+/// Some(Err(err)) instead of Err(err).
+/// ```rust
+/// assert!(try_some!(Ok(Some(1))), Some(1));
+/// assert!(try_some!(Ok(None)), None);
+/// try_some!(Err("some error")) // returns Some(Err("some error"))
+/// ```
 #[macro_export]
 macro_rules! try_some {
     ($expr:expr) => {
@@ -9,6 +16,11 @@ macro_rules! try_some {
     };
 }
 
+/// Like the try! macro only that this returns Some(Err(err)) instead of Err(err).
+/// ```rust
+/// assert!(try_err!(Ok(1)), try!(Ok(1)));
+/// assert!(try_err!(Err("some error"))) // returns Some(Err("some error"))
+/// ```
 #[macro_export]
 macro_rules! try_err {
     ($expr:expr) => {
